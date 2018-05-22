9b72e658c154d2e500bbacea767c2298659d2b1b
Ryan <jragard@gmail.com>
2018-05-22 16:36:26 -0400

    implemented 'Text' and 'App' as functional components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class Text extends Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-        color: props.color,
-        value: props.value,
-      };
-  }
-  render() {
-    return (
-      <span style ={{color: this.props.color}}>{this.props.value}</span>
-    )
-  };
+// class Text extends Component {
+//   constructor(props) {
+//       super(props);
+//       this.state = {
+//         color: props.color,
+//         value: props.value,
+//       };
+//   }
+//   render() {
+//     return (
+//       <span style={{color: this.props.color}}>{this.props.value}</span>
+//     )
+//   };
+// }
+
+function Text(props) {
+  return (
+    <span style ={{color: props.color}}>{props.value}</span>
+  );
 }
 
-
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+function App () {
+  return (
+    <div className="App">
         
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -34,10 +38,34 @@ class App extends Component {
         </p>
 
         <h1>Hi, I'm <Text color="blue" value="Ryan" /></h1>
-        
-      </div>
-    );
-  }
+
+    </div>
+  )
 }
+
+
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+        
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+
+//         <h1>Hi, I'm <Text color="blue" value="Ryan" /></h1>
+
+        
+        
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
